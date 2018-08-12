9977b4ca7aa1bfc69f88a71f5d93ab9532b672fa
K1A2 <jckim0307@gmail.com>
2018-08-13 00:21:03 +0900

    초기화

---
+++ app/src/main/java/com/contast/k1a2/vedioplayer/MainActivity.java
@@ -0,0 +1,13 @@
+package com.contast.k1a2.vedioplayer;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
